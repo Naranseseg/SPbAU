90304e38aaf5aeca1cbc464f89b11cd4d8b9c5df
Daria4549 <daria.4549@mail.ru>
2021-12-13 15:34:25 +0300

    prog_10_13dec(2).py

---
+++ prog_10_13dec.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    num_1,num_2 = a,b
+    while num_1 != 0 and num_2 != 0:
+        if num_1>num_2:
+            num_1 = num_1 % num_2
+        else:
+            num_2 = num_2 % num_1
+    return max(num_1,num_2)
+print(gcd(1,24))
+print(gcd(3,21))
