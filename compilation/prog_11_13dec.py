856531b99173bb70890001110f1321594b73a099
Daria4549 <daria.4549@mail.ru>
2022-05-03 13:24:43 +0300

    compilation

--- compilation/prog_11_13dec.py
+++ compilation/prog_11_13dec.py
@@ -1,12 +1,12 @@
 def GCD(a, b):
     multiplier = 1
-    num_1,num_2 = a,b
-    while num_1 != 0 and num_2 != 0:
-        if num_1 > num_2:
-            num_1 = num_1 % num_2
+    num1,num_2 = a,b
+    while num1 != 0 and num_2 != 0:
+        if num1 > num_2:
+            num1 = num1 % num_2
         else:
-            num_2 = num_2 % num_1
-    num_3,num_4 = a/max(num_1,num_2), b/max(num_1,num_2)
+            num_2 = num_2 % num1
+    num_3,num_4 = a/max(num1,num_2), b/max(num1,num_2)
     quotient = max(num_3,num_4)
     if num_3>num_4:
         while (num_3-1)%num_4!=0:
@@ -17,5 +17,5 @@ def GCD(a, b):
             num_4 += quotient
             multiplier += 1
     coefficient_1, coefficient_2 = multiplier, -int((multiplier*quotient-1)/min(num_3,num_4))
-    return coefficient_1,coefficient_2,max(num_1,num_2)
+    return coefficient_1,coefficient_2,max(num1,num_2)
 print(GCD(49, 42))
